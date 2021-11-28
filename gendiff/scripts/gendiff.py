fc55fae0bd6468a7c64ecc576c994808ec03cd36
Sergey Martynov <acemore@mail.ru>
2021-11-28 05:20:15 +0300

    Update gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
-from gendiff.gendiff import display_help
+import argparse
+from gendiff import generate_diff
 
 
 def main():
-    display_help()
+    gendiff_args_parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Generate diff'
+    )
+    gendiff_args_parser.add_argument('first_file')
+    gendiff_args_parser.add_argument('second_file')
+    gendiff_args_parser.add_argument(
+        '-f', '--format',
+        help='set format of output'
+    )
+    gendiff_args = gendiff_args_parser.parse_args()
+
+    print(generate_diff(gendiff_args.first_file, gendiff_args.second_file))
 
 
 if __name__ == '__main__':
