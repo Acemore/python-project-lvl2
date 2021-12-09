fcc0756a4cc6ed7fbdc23704fccef31e7096a8c1
Sergey Martynov <acemore@mail.ru>
2021-12-09 11:35:59 +0300

    Update dicts getting

--- gendiff/search_diff.py
+++ gendiff/search_diff.py
@@ -1,25 +1,17 @@
-from gendiff.formaters import (
-    ADDED, CHANGED, CHILDREN, FIRST_VALUE, NESTED,
-    REMOVED, SAME, STATE, SECOND_VALUE, VALUE
-)
+ADDED = "added"
+CHANGED = "changed"
+CHILDREN = "children"
+FIRST_VALUE = "first_value"
+NESTED = "nested"
+REMOVED = "removed"
+SAME = "same"
+SECOND_VALUE = "second_value"
+STATE = "state"
+VALUE = "value"
 
 
 def get_keys(file):
     return set(file.keys())
-
-
-def get_dict(state, first_data_key, first_data_value):
-    return {
-        STATE: state,
-        first_data_key: first_data_value
-    }
-
-
-def get_dict_for_changed_items(state, first_data_key, first_data_value,
-                               second_data_key, second_data_value):
-    result_dict = get_dict(state, first_data_key, first_data_value)
-    result_dict[second_data_key] = second_data_value
-    return result_dict
 
 
 def get_value(first_dict, second_dict, key):
@@ -29,25 +21,31 @@ def get_value(first_dict, second_dict, key):
     if type(first_dict_key_value) is dict and (
         type(second_dict_key_value) is dict
     ):
-        return get_dict(
-            NESTED,
-            CHILDREN,
-            search_diff(first_dict_key_value, second_dict_key_value)
-        )
+        return {
+            STATE: NESTED,
+            CHILDREN: search_diff(first_dict_key_value, second_dict_key_value)
+        }
     elif first_dict_key_value == second_dict_key_value:
-        return get_dict(SAME, VALUE, first_dict_key_value)
+        return {
+            STATE: SAME,
+            VALUE: first_dict_key_value
+        }
     elif key in get_keys(first_dict) and key not in get_keys(second_dict):
-        return get_dict(REMOVED, VALUE, first_dict_key_value)
+        return {
+            STATE: REMOVED,
+            VALUE: first_dict_key_value
+        }
     elif key not in get_keys(first_dict) and key in get_keys(second_dict):
-        return get_dict(ADDED, VALUE, second_dict_key_value)
+        return {
+            STATE: ADDED,
+            VALUE: second_dict_key_value
+        }
     else:
-        return get_dict_for_changed_items(
-            CHANGED,
-            FIRST_VALUE,
-            first_dict_key_value,
-            SECOND_VALUE,
-            second_dict_key_value
-        )
+        return {
+            STATE: CHANGED,
+            FIRST_VALUE: first_dict_key_value,
+            SECOND_VALUE: second_dict_key_value
+        }
 
 
 def search_diff(first_dict, second_dict):
