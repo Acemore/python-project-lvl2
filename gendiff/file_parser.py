73821fc17ee9fd383107c4f712f041e8a9207f80
Sergey Martynov <acemore@mail.ru>
2021-12-21 10:26:58 +0300

    Update file parser

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -2,10 +2,21 @@ import json
 import yaml
 
 
-def parse_file(file_path):
+def get_file_content(file_path):
+    with open(file_path) as file:
+        return file.read()
+
+
+def parse_json_file(file_raw_content):
+    return json.loads(file_raw_content)
+
+
+def parse_yaml_file(file_raw_content):
+    return yaml.safe_load(file_raw_content)
+
+
+def parse_file(file_raw_content, file_path):
     if file_path.endswith('.json'):
-        with open(file_path) as json_file:
-            return json.load(json_file)
+        return parse_json_file(file_raw_content)
     if file_path.endswith(('.yml', '.yaml')):
-        with open(file_path) as yaml_file:
-            return yaml.safe_load(yaml_file)
+        return parse_yaml_file(file_raw_content)
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,11 +1,14 @@
-from gendiff.file_parser import parse_file
+from gendiff.file_parser import get_file_content, parse_file
 from gendiff.formaters import STYLISH, select_format
 from gendiff.search_diff import search_diff
 
 
 def generate_diff(first_file_path, second_file_path, format=STYLISH):
-    first_file = parse_file(first_file_path)
-    second_file = parse_file(second_file_path)
+    first_file = parse_file(get_file_content(first_file_path), first_file_path)
+    second_file = parse_file(
+        get_file_content(second_file_path),
+        second_file_path
+    )
 
     diff = search_diff(first_file, second_file)
 
